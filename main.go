7e9dde91ca6d54dc58357a42ce16f00ebcc00f77
Joshua Hoblitt <josh@hoblitt.com>
2023-05-01 16:10:16 -0700

    add comparison of branches across repos

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ type GHRepo struct {
 	Org           string
 	Name          string
 	DefaultBranch string
-	Branches      []GHBranch
+	Branches      map[string]GHBranch
 }
 
 type GHBranch struct {
@@ -38,44 +38,46 @@ type GHBranch struct {
 }
 
 func NewGHRepo(ctx context.Context, client *github.Client, org, name string) GHRepo {
-	repo := GHRepo{
-		Org:  org,
-		Name: name,
+	r := GHRepo{
+		Org:      org,
+		Name:     name,
+		Branches: make(map[string]GHBranch),
 	}
 
-	repo.DefaultBranch = get_default_branch(ctx, client, repo)
-	fmt.Printf("%s/%s default branch is: %s\n", repo.Org, repo.Name, repo.DefaultBranch)
+	r.DefaultBranch = get_default_branch(ctx, client, r)
+	fmt.Printf("%s default branch is: %s\n", r.FullName(), r.DefaultBranch)
 
-	branches, _, err := client.Repositories.ListBranches(ctx, repo.Org, repo.Name, nil)
+	branches, _, err := client.Repositories.ListBranches(ctx, r.Org, r.Name, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var branch_info []GHBranch
 	for _, b := range branches {
 		branch_name := *b.Name
 
 		// skip comparing the default branch against itself
-		if branch_name == repo.DefaultBranch {
+		if branch_name == r.DefaultBranch {
 			continue
 		}
 
 		// compare branch against the default branch
-		compare, _, err := client.Repositories.CompareCommits(ctx, repo.Org, repo.Name, repo.DefaultBranch, branch_name, nil)
+		compare, _, err := client.Repositories.CompareCommits(ctx, r.Org, r.Name, r.DefaultBranch, branch_name, nil)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		branch_info = append(branch_info, GHBranch{
+		r.Branches[branch_name] = GHBranch{
 			Name:     branch_name,
 			AheadBy:  compare.GetAheadBy(),
 			BehindBy: compare.GetAheadBy(),
-		})
+		}
 	}
 
-	repo.Branches = branch_info
+	return r
+}
 
-	return repo
+func (r GHRepo) FullName() string {
+	return fmt.Sprintf("%s/%s", r.Org, r.Name)
 }
 
 func gh_client(ctx context.Context, gh_token string) *github.Client {
@@ -114,6 +116,13 @@ func get_default_branch(ctx context.Context, client *github.Client, repo GHRepo)
 	return r.GetDefaultBranch()
 }
 
+// https://stackoverflow.com/a/57213476
+func RemoveIndex(s []GHRepo, index int) []GHRepo {
+	ret := make([]GHRepo, 0)
+	ret = append(ret, s[:index]...)
+	return append(ret, s[index+1:]...)
+}
+
 func main() {
 	gh_token := os.Getenv("GITHUB_TOKEN")
 	if gh_token == "" {
@@ -129,11 +138,31 @@ func main() {
 		project_repos = append(project_repos, NewGHRepo(ctx, client, r.OrgName, r.RepoName))
 	}
 
-	for _, r := range project_repos {
-		var notta_branches []string
+	safe_branches := map[string]GHRepo{}
+
+	for i, r := range project_repos {
+		other_repos := RemoveIndex(project_repos, i)
+
 	BRANCH:
 		for _, b := range r.Branches {
+			fmt.Printf("%s/%s:", r.Org, r.Name)
 			fmt.Println(b.Name, "--", "ahead by:", b.AheadBy, "behind by", b.BehindBy)
+
+			// Check to see if this branch is already known to be ahead.
+			safer, ok := safe_branches[b.Name]
+			if ok {
+				fmt.Println("ignoring branch:", b.Name, "because it is known to be ahead in", safer.FullName())
+				continue BRANCH
+			}
+
+			// if this branch is ahead of the default branch, we want to preserve it
+			// across all repos, even if it not ahead of the default branch in other
+			// repo(s).
+			if b.AheadBy != 0 {
+				fmt.Println("ignoring branch:", b.Name, "because it is ahead")
+				safe_branches[b.Name] = r
+				continue BRANCH
+			}
 
 			exclude := conf.ExcludePatterns
 			for _, pattern := range exclude {
@@ -142,19 +171,27 @@ func main() {
 					log.Fatal(err)
 				}
 				if match == true {
+					// This branch isn't being added to safe_branches so we can report if
+					// a branch is ignored because it is ahead in a different repo or it
+					// has matched an exclude_pattern.
 					fmt.Println("ignoring branch:", b.Name, "because it matched exclude_pattern:", pattern)
 					continue BRANCH
 				}
 			}
 
-			if b.AheadBy == 0 {
-				notta_branches = append(notta_branches, b.Name)
-			}
-		}
+			// Check for this branch in other repos. If any repo has this branch
+			// ahead of the default branch, then this branch name is considered safe
+			// across all repos and there's no need to keep checking any additional
+			// repos.
+			for _, otherr := range other_repos {
+				otherb, ok := otherr.Branches[b.Name]
 
-		fmt.Println("branches with are not ahead")
-		for _, b := range notta_branches {
-			fmt.Println(b)
+				if ok && otherb.AheadBy != 0 {
+					fmt.Println("ignoring branch:", b.Name, "because it is ahead in", otherr.FullName())
+					safe_branches[b.Name] = r
+					continue BRANCH
+				}
+			}
 		}
 	}
 }
