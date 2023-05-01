f3404553a7a27b5d880649c888e2eff04c43301a
Joshua Hoblitt <josh@hoblitt.com>
2023-05-01 12:21:57 -0700

    add aborist.yaml config file

--- go.mod
+++ go.mod
@@ -3,18 +3,19 @@ module github.com/jhoblitt/arborist
 go 1.19
 
 require (
+	github.com/gofri/go-github-ratelimit v1.0.3
+	github.com/google/go-github/v52 v52.0.0
+	golang.org/x/oauth2 v0.7.0
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
 	github.com/ProtonMail/go-crypto v0.0.0-20230217124315-7d5c6f04bbb8 // indirect
 	github.com/cloudflare/circl v1.1.0 // indirect
-	github.com/gofri/go-github-ratelimit v1.0.3 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-github/v52 v52.0.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
-	github.com/kr/pretty v0.3.1 // indirect
-	github.com/kr/text v0.2.0 // indirect
-	github.com/rogpeppe/go-internal v1.9.0 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/net v0.9.0 // indirect
-	golang.org/x/oauth2 v0.7.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"regexp"
@@ -10,7 +11,14 @@ import (
 	"github.com/gofri/go-github-ratelimit/github_ratelimit"
 	"github.com/google/go-github/v52/github"
 	"golang.org/x/oauth2"
+	"gopkg.in/yaml.v3"
 )
+
+type ArboristConf struct {
+	OrgName        string   `yaml:"org_name"`
+	RepoName       string   `yaml:"repo_name"`
+	ExcludePattern []string `yaml:"exclude_pattern"`
+}
 
 func gh_client(ctx context.Context, gh_token string) *github.Client {
 	ts := oauth2.StaticTokenSource(
@@ -25,10 +33,31 @@ func gh_client(ctx context.Context, gh_token string) *github.Client {
 	return github.NewClient(rateLimiter)
 }
 
+func parse_conf_file() ArboristConf {
+	raw_conf, err := ioutil.ReadFile("arborist.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var conf ArboristConf
+	err = yaml.Unmarshal(raw_conf, &conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return conf
+}
+
 func main() {
-	org_name := "lsst-it"
-	repo_name := "lsst-control"
 	gh_token := os.Getenv("GITHUB_TOKEN")
+	if gh_token == "" {
+		log.Fatal("GITHUB_TOKEN env var is not defined")
+	}
+
+	conf := parse_conf_file()
+
+	org_name := conf.OrgName
+	repo_name := conf.RepoName
 
 	ctx := context.Background()
 	client := gh_client(ctx, gh_token)
@@ -46,29 +75,38 @@ func main() {
 	}
 
 	var notta_branches []string
+
+BRANCH:
 	for _, b := range branches {
-		name := *b.Name
+		branch_name := *b.Name
 
 		// skip comparing the default branch against itself
-		if *b.Name == default_branch {
+		if branch_name == default_branch {
 			continue
 		}
 
 		// compare branch against the default branch
-		compare, _, err := client.Repositories.CompareCommits(ctx, org_name, repo_name, default_branch, name, nil)
+		compare, _, err := client.Repositories.CompareCommits(ctx, org_name, repo_name, default_branch, branch_name, nil)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Println(*b.Name, "--", "ahead by:", compare.GetAheadBy(), "behind by", compare.GetBehindBy())
+		fmt.Println(branch_name, "--", "ahead by:", compare.GetAheadBy(), "behind by", compare.GetBehindBy())
 
-		match, err := regexp.MatchString("production", name)
-		if match == true {
-			continue
+		exclude := conf.ExcludePattern
+		for _, pattern := range exclude {
+			match, err := regexp.MatchString(pattern, branch_name)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if match == true {
+				fmt.Println("ignoring branch:", branch_name, "because it matched exclude_pattern:", pattern)
+				continue BRANCH
+			}
 		}
 
 		if compare.GetAheadBy() == 0 {
-			notta_branches = append(notta_branches, name)
+			notta_branches = append(notta_branches, branch_name)
 		}
 	}
 
